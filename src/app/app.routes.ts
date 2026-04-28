aca59061d62636e569389449f9766db7392307cd
FazioNico <contact@nicolasfazio.ch>
2026-04-28 09:45:31 +0200

    feat: add router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { OrderPage } from './pages/order-page/order-page';
+import { AdminPage } from './pages/admin-page/admin-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'order-page',
+    component: OrderPage
+  },
+  {
+    path: 'admin',
+    component: AdminPage
+  },
+  {
+    path: '',
+    redirectTo: 'order-page',
+    pathMatch: 'full'
+  },
+];
--- src/app/pages/order-page/order-page.ts
+++ src/app/pages/order-page/order-page.ts
@@ -1,14 +1,21 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { Category } from '../../interfaces';
 import { ApiService } from '../../services/api-service/api-service';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-order-page',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './order-page.html',
   styleUrl: './order-page.css',
 })
-export class OrderPage {
+export class OrderPage implements OnInit {
   protected readonly categories = inject(ApiService).categories;
+  protected readonly route = inject(ActivatedRoute);
+
+  ngOnInit(): void {
+    const params = this.route.snapshot.queryParams;
+    console.log(params);
+  }
 }
