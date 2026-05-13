b3f19063dad6c989b5171b131a34c25d94e9d7b3
FazioNico <contact@nicolasfazio.ch>
2026-05-13 12:12:22 +0200

    feat: add recipe selection functionality to checkout page

--- src/app/pages/checkout-page/checkout-page.ts
+++ src/app/pages/checkout-page/checkout-page.ts
@@ -1,8 +1,10 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { OrderService } from '../../services/order/order-service';
 import { TotalOrderPipe } from '../../pipes/total-order/total-order-pipe';
 import { CurrencyPipe } from '@angular/common';
 import { Router, RouterLink } from "@angular/router";
+import { ApiService } from '../../services/api-service/api-service';
+import { Category, Recipe } from '../../interfaces';
 
 @Component({
   selector: 'app-checkout-page',
@@ -13,7 +15,16 @@ import { Router, RouterLink } from "@angular/router";
 export class CheckoutPage {
   private readonly _router = inject(Router);
   private readonly _orderService = inject(OrderService);
+  private readonly _apiService = inject(ApiService);
   public readonly orderForm = this._orderService.orderForm;
+  public readonly category = computed<Category|undefined>(() => {
+    const categories = this._apiService.categories();
+    return categories.find(category => category.uuid  === '6ad53b55-60c7-5cb9-b546-32df543a4f5d');
+  });
+
+  async handleAddRecipe(recipeId: string) {
+    await this._orderService.addRecipe(this.category()!.recipes, recipeId);
+  }
 
   async handleSubmitOrder() {
     const orderId = await this._orderService.submitOrder();
