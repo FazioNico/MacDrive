b2eed739006ed6f82621ada5257a8355c9b18d6f
FazioNico <contact@nicolasfazio.ch>
2026-05-21 12:52:29 +0200

    feat: enhance success page layout and functionality with improved styling and order details

--- src/app/pages/success-page/success-page.ts
+++ src/app/pages/success-page/success-page.ts
@@ -1,13 +1,24 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
-import { ActivatedRoute, Router, RouterLink } from '@angular/router';
-import { IonContent } from '@ionic/angular/standalone';
-import JSConfetti from 'js-confetti'
+import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCol, IonContent, IonGrid, IonItem, IonLabel, IonList, IonRow } from '@ionic/angular/standalone';
+import JSConfetti from 'js-confetti';
 
 const IonElements = [
-  IonContent
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonRow,
 ];
 @Component({
   selector: 'app-success-page',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [RouterLink, ...IonElements],
   templateUrl: './success-page.html',
   styleUrl: './success-page.css',
