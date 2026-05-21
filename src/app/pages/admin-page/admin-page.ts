8d6438ee1a1950ea01e1a4588ef5207c5de2a3cd
FazioNico <contact@nicolasfazio.ch>
2026-05-21 12:57:06 +0200

    feat: enhance admin and order detail pages with improved layout, styling, and functionality

--- src/app/pages/admin-page/admin-page.ts
+++ src/app/pages/admin-page/admin-page.ts
@@ -1,15 +1,27 @@
-import { Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { FireService } from '../../services/fire/fire-service';
 import { AsyncPipe, CurrencyPipe } from '@angular/common';
 import { TotalOrderPipe } from '../../pipes/total-order/total-order-pipe';
-import { IonContent } from '@ionic/angular/standalone';
+import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCol, IonContent, IonGrid, IonItem, IonLabel, IonList, IonRow, IonSpinner } from '@ionic/angular/standalone';
 
 const IonElements = [
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCol,
   IonContent,
+  IonGrid,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonRow,
+  IonSpinner,
 ];
 @Component({
   selector: 'app-admin-page',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [RouterLink, AsyncPipe, TotalOrderPipe, CurrencyPipe, ...IonElements],
   templateUrl: './admin-page.html',
   styleUrl: './admin-page.css',
--- src/app/pages/order-detail/order-detail.ts
+++ src/app/pages/order-detail/order-detail.ts
@@ -1,21 +1,33 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { OrderDataInterface } from '../../services/fire/fire-service';
-import { IonContent } from '@ionic/angular/standalone';
+import { CurrencyPipe } from '@angular/common';
+import { TotalOrderPipe } from '../../pipes/total-order/total-order-pipe';
+import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCol, IonContent, IonGrid, IonItem, IonLabel, IonList, IonRow } from '@ionic/angular/standalone';
 
 const IonElements = [
-  IonContent
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonRow,
 ];
 @Component({
   selector: 'app-order-detail',
-  imports: [RouterLink, ...IonElements],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [RouterLink, CurrencyPipe, TotalOrderPipe, ...IonElements],
   templateUrl: './order-detail.html',
   styleUrl: './order-detail.css',
 })
 export class OrderDetail implements OnInit {
   protected readonly route = inject(ActivatedRoute);
-  protected readonly orderId = signal<string|undefined>(undefined);
-  protected readonly orderDetail = signal<OrderDataInterface| undefined>(undefined);
+  protected readonly orderDetail = signal<OrderDataInterface | undefined>(undefined);
   
   ngOnInit(): void {
     const orderDetail = this.route.snapshot.data['orderDetail'];
