224e656cb073bc390ab0c2fce39677b73d41d91b
FazioNico <contact@nicolasfazio.ch>
2026-05-27 12:39:00 +0200

    feat: add platform detection

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,6 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA, inject, OnInit, signal } from '@angular/core';
 import { Auth, signInAnonymously } from '@angular/fire/auth';
-import { IonRouterOutlet } from '@ionic/angular/standalone';
+import { IonRouterOutlet, Platform } from '@ionic/angular/standalone';
 import { UpdatesNotificationComponent } from './app-updates-notification';
 
 @Component({
@@ -13,8 +13,11 @@ import { UpdatesNotificationComponent } from './app-updates-notification';
 export class App implements OnInit {
   public readonly title = signal('MacDrive');
   private readonly _auth = inject(Auth);
+  private readonly _platform = inject(Platform);
 
   async ngOnInit() {
+    const p = this._platform.platforms();
+    console.log('Current platform: ', p);
     // you can use ngOnInit if you have to run multiple async logic
     // await signInAnonymously(this._auth);
     // await myNextAsyncMethod();
